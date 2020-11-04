c003912c1fcc87a141800ed64e387c04f34e273d
Nikita Golik <nikitq4a@gmail.com>
2020-11-04 12:48:02 +0300

    executor

---
+++ ThreadExamples/19ExecutorServiceCallable/src/main/java/by/golik/CalcCallable.java
@@ -0,0 +1,15 @@
+package by.golik;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+/**
+ * @author Nikita Golik
+ */
+public class CalcCallable implements Callable<Number> {
+    @Override
+    public Number call() throws Exception {
+        Number res = new Random().nextGaussian(); // имитация вычислений
+        return res;
+    }
+}
---
+++ ThreadExamples/19ExecutorServiceCallable/src/main/java/by/golik/ProductList.java
@@ -0,0 +1,21 @@
+package by.golik;
+
+import java.util.ArrayDeque;
+
+/**
+ * @author Nikita Golik
+ */
+public class ProductList {
+    private static ArrayDeque<String> arr = new ArrayDeque<String>() {
+        {
+            this.add("Product 1");
+            this.add("Product 2");
+            this.add("Product 3");
+            this.add("Product 4");
+            this.add("Product 5");
+        }
+    };
+    public static String getProduct() {
+        return arr.poll();
+    }
+}
