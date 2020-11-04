328d1613ef5ccc0aefe9c6b939fad449bb5d7b5b
Nikita Golik <nikitq4a@gmail.com>
2020-11-04 12:52:28 +0300

    exchanger

---
+++ ThreadExamples/20ThreadExchanger/src/main/java/by/golik/GetThread.java
@@ -0,0 +1,29 @@
+package by.golik;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @author Nikita Golik
+ */
+public class GetThread implements Runnable {
+    Exchanger<String> exchanger;
+    String message;
+
+    GetThread(Exchanger ex){
+
+        this.exchanger=ex;
+        message = "Привет мир!";
+    }
+    public void run(){
+
+        try{
+            message += " - It is the message from GetThread (" + Thread.currentThread().getName() + ")";
+            message=exchanger.exchange(message);
+            System.out.println("GetThread " + Thread.currentThread().getName() + " получил: " + message);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
---
+++ ThreadExamples/20ThreadExchanger/src/main/java/by/golik/PutThread.java
@@ -0,0 +1,28 @@
+package by.golik;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @author Nikita Golik
+ */
+public class PutThread implements Runnable {
+    Exchanger<String> exchanger;
+    String message;
+
+    PutThread(Exchanger ex){
+
+        this.exchanger=ex;
+        message = "Hello Java!";
+    }
+    public void run(){
+
+        try{
+            message += " - It is the message from PutThread (" + Thread.currentThread().getName() + ")";
+            message=exchanger.exchange(message);
+            System.out.println("PutThread " + Thread.currentThread().getName() + " получил: " + message);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
