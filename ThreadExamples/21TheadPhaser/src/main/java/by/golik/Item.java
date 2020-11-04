89f7e9a4434fd981a330a413934aae7d92d41f40
Nikita Golik <nikitq4a@gmail.com>
2020-11-04 13:08:56 +0300

    atomic

---
+++ ThreadExamples/21TheadPhaser/src/main/java/by/golik/Item.java
@@ -0,0 +1,14 @@
+package by.golik;
+
+/**
+ * @author Nikita Golik
+ */
+public class Item {
+    private int registrationNumber;
+    public Item(int number) {
+        this.registrationNumber = number;
+    }
+    public int getRegistrationNumber() {
+        return registrationNumber;
+    }
+}
---
+++ ThreadExamples/22Atomic/src/main/java/by/golik/ThreadRunner.java
@@ -0,0 +1,49 @@
+package by.golik;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
+
+/**
+ * @author Nikita Golik
+ */
+public class ThreadRunner {
+
+    public static void main(String[] args) {
+        final AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        new Thread("Thread 1") {
+
+            public void run() {
+
+                while(true) {
+                    System.out.println(Thread.currentThread().getName()
+                            +" Waiting for Thread 2 to set Atomic variable to true. Current value is "
+                            + atomicBoolean.get());
+
+                    if(atomicBoolean.compareAndSet(true, false)) {
+                        System.out.println("Done!");
+                        break;
+                    }
+                }
+            };
+        }.start();
+
+        new Thread("Thread 2") {
+
+            public void run() {
+                System.out.println(Thread.currentThread().getName() +
+                        ", Atomic Variable: " +atomicBoolean.get());
+                System.out.println(Thread.currentThread().getName() +
+                        " is setting the variable to true ");
+                atomicBoolean.set(true);
+                System.out.println(Thread.currentThread().getName() +
+                        ", Atomic Variable: " +atomicBoolean.get());
+            };
+        }.start();
+    }
+}
