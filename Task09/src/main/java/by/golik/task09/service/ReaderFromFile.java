e245b1cdb3f0958d114202f810572b513e07a739
Nikita Golik <nikitq4a@gmail.com>
2020-11-16 09:20:51 +0300

    parser

---
+++ Task09/src/main/java/by/golik/task09/service/ReaderFromFile.java
@@ -0,0 +1,30 @@
+package by.golik.task09.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ * @author Nikita Golik
+ */
+public class ReaderFromFile {
+
+    private static Logger logger = LogManager.getLogger(ReaderFromFile.class);
+
+    public static String getData(String filePath) {
+        File file = new File(filePath);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (IOException e) {
+            logger.error(e);
+        }
+        return builder.toString();
+    }
+}
---
+++ Task09/src/main/java/by/golik/task09/service/handler/BaseInterface.java
@@ -0,0 +1,8 @@
+package by.golik.task09.service.handler;
+
+/**
+ * @author Nikita Golik
+ */
+public interface BaseInterface {
+    public String handleRequest();
+}
