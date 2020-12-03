b5959f86f8bdc2c67e39283e057625ddad907c50
Nikita Golik <nikitq4a@gmail.com>
2020-12-03 20:18:48 +0300

    16 elements

--- task10 web/src/test/java/FlowerSAXBuilderTest.java
+++ task10 web/src/test/java/FlowerSAXBuilderTest.java
@@ -1,5 +1,21 @@
+import by.golik.builder.AbstractFlowerBuilder;
+import by.golik.builder.FlowerBuilderFactory;
+import by.golik.builder.FlowerDOMBuilder;
+import by.golik.exception.IncorrectInputException;
+import by.golik.exception.ParserException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * @author Nikita Golik
  */
 public class FlowerSAXBuilderTest {
+    @Test
+    public void parse() throws ParserException, IncorrectInputException {
+        FlowerBuilderFactory flowerBuilderFactory = new FlowerBuilderFactory();
+        AbstractFlowerBuilder abstractFlowerBuilder = flowerBuilderFactory.createFlowerBuilder("SAX");
+        abstractFlowerBuilder.buildFlowerList("data/flowers.xml");
+
+        Assert.assertEquals(abstractFlowerBuilder.getFlowerSet().size(), 16);
+    }
 }
